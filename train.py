5d9c385355ab5768401c3dade6239955a3f980fa
zwlanpishu <zhaowei.0817@163.com>
2020-05-10 19:03:33 +0800

    1. remove tensorflow dependency. Warning: Stable version.

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from model import Tacotron2
 from data_utils import TextMelLoader, TextMelCollate
 from loss_function import Tacotron2Loss
 from logger import Tacotron2Logger
-from hparams import create_hparams
+import hparams
 
 
 def lr_decay(learning_rate, iteration):
@@ -403,15 +403,8 @@ if __name__ == "__main__":
         required=False,
         help="Distributed group name",
     )
-    parser.add_argument(
-        "--hparams",
-        type=str,
-        required=False,
-        help="comma separated name=value pairs",
-    )
 
     args = parser.parse_args()
-    hparams = create_hparams(args.hparams)
 
     torch.backends.cudnn.enabled = hparams.cudnn_enabled
     torch.backends.cudnn.benchmark = hparams.cudnn_benchmark
